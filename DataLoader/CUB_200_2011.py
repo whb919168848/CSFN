0f5ded5e6c847aaa71abb01dae06488b52b90ab9
Wen1210 <157097606+whb919168848@users.noreply.github.com>
2025-05-05 09:33:38 +0800

    Add files via upload

---
+++ DataLoader/CUB_200_2011.py
@@ -0,0 +1,131 @@
+import torch
+import os
+import torch.nn as nn 
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np 
+import torch.utils.data as data
+from torchvision.datasets import ImageFolder
+import pandas as pd
+import json
+
+def load_test_bbox(root, test_txt_path, test_gt_path,resize_size, crop_size):
+    test_gt = []
+    test_txt = []
+    shift_size = (resize_size - crop_size) // 2.
+    with open(test_txt_path, 'r') as f:
+        for line in f:
+            img_path = line.strip('\n').split(';')[0]
+            # img_path = line.strip('\n').split(' ')[1]
+            test_txt.append(img_path)
+    with open(test_gt_path, 'r') as f:        
+        for line in f:
+            x0, y0, x1, y1, h, w = line.strip('\n').split(' ')
+            x0, y0, x1, y1, h, w = float(x0), float(y0), float(x1), float(y1), float(h), float(w)
+            # x1 = x1 + x0 #
+            # y1 = y1 + y0 #
+            x0 = int(max(x0 / w * resize_size - shift_size, 0))
+            y0 = int(max(y0 / h * resize_size - shift_size, 0))
+            x1 = int(min(x1 / w * resize_size - shift_size, crop_size - 1))
+            y1 = int(min(y1 / h * resize_size - shift_size, crop_size - 1))
+            test_gt.append(np.array([x0, y0, x1, y1]).reshape(-1))
+    final_dict = {}
+    for k, v in zip(test_txt, test_gt):
+        k = os.path.join(root, 'test', k)
+        k = k.replace('/', '\\')
+        final_dict[k] = v
+    return final_dict
+
+class ImageDataset(data.Dataset):
+    def __init__(self, args ,phase=None):
+        args.num_classes = 200
+        self.args =args
+        self.load_class_path = '/home/dell/data/code/whb/SAT-main(CLIP)/Model/cub_efficientnetb7.json'
+        self.root = '/home/dell/data/dataset/CUB_200_2011'
+        # self.root = '/home/dell/data/dataset/CUB_200_2011/split'
+        self.test_txt_path = self.root + '/' + 'test_list.txt'
+        self.test_gt_path = self.root + '/' + 'test_bounding_box.txt'
+        # self.test_txt_path = self.root + '/' + 'test4.txt'
+        # self.test_gt_path = self.root + '/' + 'test_box4.txt'
+        self.crop_size = args.crop_size 
+        self.resize_size = args.resize_size
+        self.phase = args.phase if phase == None else phase
+        self.num_classes = args.num_classes
+        self.tencrop = args.tencrop
+        if self.phase == 'train':
+            self.img_dataset = ImageFolder(os.path.join(self.root, 'train'))
+            self.transform = transforms.Compose([
+                transforms.Resize((self.resize_size, self.resize_size)),
+                transforms.RandomCrop(self.crop_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        elif self.phase == 'test':
+            self.img_dataset = ImageFolder(os.path.join(self.root, 'test'))
+            self.transform = transforms.Compose([
+                transforms.Resize((self.resize_size, self.resize_size)),
+                transforms.CenterCrop(self.crop_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+            self.transform_tencrop = transforms.Compose([
+                transforms.Resize((self.resize_size, self.resize_size)),
+                transforms.TenCrop(args.crop_size),
+                transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+                transforms.Lambda(lambda crops: torch.stack([transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))(crop) for crop in crops])),
+            ])
+         
+        self.img_dataset = self.img_dataset.imgs   
+        self.test_bbox = load_test_bbox(self.root, self.test_txt_path, self.test_gt_path,self.resize_size, self.crop_size)
+
+        #new add
+        self.pd_file = pd.read_csv("/home/dell/data/dataset/CUB_200_2011/cub_bird_train.txt", sep=" ", header=None,
+                               names=['ImageName', 'label'])
+
+        with open(self.load_class_path, 'r') as f:
+            self.name2result = json.load(f)
+
+        # self.name2result = np.load('/home/dell/data/code/whb/CSFN/cub_test4.npy', allow_pickle=True).item()
+
+
+    def __getitem__(self, index):
+        path, img_class = self.img_dataset[index]
+        img = Image.open(path).convert('RGB')      
+        img_trans = self.transform(img)  
+        if self.phase == 'train':
+            # new add
+            positive_image = self.fetch_positive(1, img_class, path.split('train')[-1])[0]
+            return path, img_trans, img_class, positive_image
+        else:
+            name = path.split('test')[1][1:]
+
+            path = path.replace('/', '\\')
+            # path = path.replace('test1', 'test') #
+            bbox = self.test_bbox[path]
+
+            pre_lable = self.name2result[name]['pred_label']
+            # pre_lable = self.name2result[name]
+            if self.tencrop:
+                img_tencrop = self.transform_tencrop(img) 
+                return img_trans, img_tencrop, img_class, bbox, path, pre_lable
+            return img_trans, img_trans, img_class, bbox, path, pre_lable
+
+    def __len__(self):
+        return len(self.img_dataset)
+
+    def fetch_positive(self, num_positive, label, path): #正样本数, 正样本类别标签, txt中源图像路径名
+        path = 'images' + path
+        other_img_info = self.pd_file[(self.pd_file.label == label) & (self.pd_file.ImageName != path)]
+        other_img_info = other_img_info.sample(min(num_positive, len(other_img_info))).to_dict('records')
+        other_img_path = [os.path.join('/home/dell/data/dataset/CUB_200_2011', e['ImageName']) for e in other_img_info]
+        other_img = [self.pil_loader(img) for img in other_img_path]
+        positive_img = [self.transform(img) for img in other_img]
+        return positive_img
+
+    def pil_loader(self,imgpath):
+        with open(imgpath, 'rb') as f:
+            with Image.open(f) as img:
+                return img.convert('RGB')
+
---
+++ DataLoader/ILSVRC.py
@@ -0,0 +1,127 @@
+import torch
+import os
+import torch.nn as nn 
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np 
+import torch.utils.data as data
+from torchvision.datasets import ImageFolder
+import pandas as pd
+import json
+
+def load_test_bbox(root, test_gt_path,crop_size,resize_size):
+    test_gt = []
+    test_txt = []
+    shift_size = (resize_size - crop_size) // 2
+    with open(test_gt_path, 'r') as f:
+        
+        for line in f:
+            temp_gt = []
+            part_1, part_2 = line.strip('\n').split(';')
+            img_path, w, h, _ = part_1.split(' ')
+            part_2 = part_2[1:]
+            bbox = part_2.split(' ')
+            bbox = np.array(bbox, dtype=np.float32)
+            box_num = len(bbox) // 4
+            w, h = np.float32(w),np.float32(h)
+            for i in range(box_num):
+                bbox[4*i] = int(max(bbox[4*i] / w * resize_size - shift_size, 0))
+                bbox[4*i+1] = int(max(bbox[4*i+1] / h * resize_size - shift_size, 0))
+                bbox[4*i+2] = int(min(bbox[4*i+2] / w * resize_size - shift_size, crop_size - 1))
+                bbox[4*i+3] = int(min(bbox[4*i+3] / h * resize_size - shift_size, crop_size - 1))
+                temp_gt.append([bbox[4*i], bbox[4*i+1], bbox[4*i+2], bbox[4*i+3]])
+            test_gt.append(temp_gt)
+            img_path = img_path.replace("\\\\","\\")
+            test_txt.append(img_path)
+    final_dict = {}
+    for k, v in zip(test_txt, test_gt):
+        k = os.path.join(root, 'val', k)
+        k = k.replace('/', '\\')
+        final_dict[k] = v
+    return final_dict
+
+class ImageDataset(data.Dataset):
+    def __init__(self, args, phase=None):
+        args.num_classes = 1000
+        self.args =args
+        # self.load_class_path = '/home/dell/data/code/whb/pretrained/imagenet_conformer-base-p16_3rdparty_8xb128_in1k.json'
+        self.load_class_path = '/home/dell/data/code/whb/pretrained/imagenet_efficientnet-b7_3rdparty_8xb32-aa-advprop_in1k.json'
+        self.root = '/home/dell/data/dataset/ImageNet_2012/'
+        self.test_txt_path = '/home/dell/data/code/whb/SAT-main/Data/ILSVRC/val_list.txt'
+        self.test_gt_path = '/home/dell/data/code/whb/SAT-main/Data/ILSVRC/val_gt.txt'
+        self.crop_size = args.crop_size
+        self.resize_size = args.resize_size
+        self.phase = args.phase if phase == None else phase
+        self.num_classes = args.num_classes
+        self.tencrop = args.tencrop
+        if self.phase == 'train':
+            self.img_dataset = ImageFolder(os.path.join(self.root, 'train'))
+            self.transform = transforms.Compose([
+                transforms.Resize((self.resize_size, self.resize_size)),
+                transforms.RandomCrop(self.crop_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        elif self.phase == 'test':
+            self.img_dataset = ImageFolder(os.path.join(self.root, 'val1'))
+            self.transform = transforms.Compose([
+                transforms.Resize((self.resize_size, self.resize_size)),
+                transforms.CenterCrop(self.crop_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+            self.transform_tencrop = transforms.Compose([
+                    transforms.Resize((self.resize_size, self.resize_size)),
+                    transforms.TenCrop(args.crop_size),
+                    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+                    transforms.Lambda(lambda crops: torch.stack([transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))(crop) for crop in crops])),
+                ]) 
+        self.img_dataset = self.img_dataset.imgs    
+        self.test_bbox = load_test_bbox(self.root, self.test_gt_path,self.crop_size,self.resize_size)
+        # new add
+        self.pd_file = pd.read_csv("/home/dell/data/dataset/ImageNet_2012/train.txt", sep=" ", header=None,
+                                   names=['ImageName', 'label'])
+
+        with open(self.load_class_path, 'r') as f:
+            self.name2result = json.load(f)
+
+    def __getitem__(self, index):
+        path, img_class = self.img_dataset[index]
+          
+        img = Image.open(path).convert('RGB')
+        img_trans = self.transform(img)
+        if self.phase == 'train':
+            positive_image = self.fetch_positive(1, img_class, path.split('train')[-1])[0]
+            return path, img_trans, img_class, positive_image
+        else:
+            name = path.split('val1/')[1][0:-5]
+            path = path.replace('val1', 'val')
+            path = path.replace('/', '\\')
+            bbox = self.test_bbox[path]
+            bbox = np.array(bbox).reshape(-1)
+            bbox = " ".join(list(map(str, bbox)))
+
+            pre_lable = self.name2result[name]['pred_label']
+            if self.tencrop:
+                img_tencrop = self.transform_tencrop(img) 
+                return img_trans, img_tencrop, img_class, bbox, path, pre_lable
+            return img_trans, img_trans, img_class, bbox, path, pre_lable
+
+    def __len__(self):
+        return len(self.img_dataset)
+
+    def fetch_positive(self, num_positive, label, path): #正样本数, 正样本类别标签, txt中源图像路径名
+        path = path[1:]
+        other_img_info = self.pd_file[(self.pd_file.label == label) & (self.pd_file.ImageName != path)]
+        other_img_info = other_img_info.sample(min(num_positive, len(other_img_info))).to_dict('records')
+        other_img_path = [os.path.join('/home/dell/data/dataset/ImageNet_2012/train', e['ImageName']) for e in other_img_info]
+        other_img = [self.pil_loader(img) for img in other_img_path]
+        positive_img = [self.transform(img) for img in other_img]
+        return positive_img
+
+    def pil_loader(self,imgpath):
+        with open(imgpath, 'rb') as f:
+            with Image.open(f) as img:
+                return img.convert('RGB')
