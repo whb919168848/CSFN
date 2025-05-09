cacbe978976abd1ecb08416e3de0b43343137b57
Wen1210 <157097606+whb919168848@users.noreply.github.com>
2025-05-09 18:41:10 +0800

    Delete DataLoader directory

--- DataLoader/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import *
-
-__all__ = ['CUB_200_2011',
-           'OpenImage',
-           'Fgvc_aircraft_2013b',
-           'Stanford_Dogs',
-           'Standford_Car',
-           'ILSVRC']
